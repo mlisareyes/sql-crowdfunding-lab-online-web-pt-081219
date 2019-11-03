bb170adccdb5543ae06c5440997d5d36255fb544
Monica R <mlisareyes@gmail.com>
2019-11-03 22:03:52 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,10 +1,12 @@
 CREATE TABLE projects (
-  id INTEGER
-   PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   category TEXT,
   funding_goal INTEGER,
   start_date TEXT,
   end_date TEXT
-
 );
+
+CREATE TABLE users (
+
+)
